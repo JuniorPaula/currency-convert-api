0dcb5a36a5e10e8783c4c9ac3bfd3737e1bca93a
Junior Paula <luke.junnior@icloud.com>
2023-01-11 18:09:00 -0300

    feat: ensure TransactionController call TransactionsUsecase with correct values

--- src/presentation/controllers/transactions-controller.ts
+++ src/presentation/controllers/transactions-controller.ts
@@ -4,10 +4,14 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { HttpStatusCodes } from '../utils/http-status-codes'
 
 export class TransactionController implements Controller {
+  constructor(private readonly transactionsUsecase) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { userId } = httpRequest.params
     if (!userId) {
       return HttpStatusCodes.badRequest(new MissingParamError('userId'))
     }
+
+    await this.transactionsUsecase.load({ userId })
   }
 }
