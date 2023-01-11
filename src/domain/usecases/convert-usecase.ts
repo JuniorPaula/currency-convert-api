217a4eb42967080f1f513273d9a69af584588bc0
Junior Paula <luke.junnior@icloud.com>
2023-01-11 14:28:16 -0300

    feat: ensure ConvertUsecase call ConvertRepository with correct values

--- src/domain/usecases/convert-usecase.ts
+++ src/domain/usecases/convert-usecase.ts
@@ -1,11 +1,36 @@
 export class ConvertUsecase {
-  constructor(private readonly apiLayerService) {}
+  constructor(
+    private readonly apiLayerService,
+    private readonly convertRepository,
+  ) {}
 
   async convert({ userId, originCurrency, originAmount, destinationCurrency }) {
-    await this.apiLayerService.execute({
+    const result = await this.apiLayerService.execute({
       originCurrency,
       originAmount,
       destinationCurrency,
     })
+
+    await this.convertRepository.save({
+      userId,
+      originCurrency,
+      originAmount,
+      destinationCurrency,
+      currencyTax: result.quote,
+      timeConvert: new Date(),
+    })
+
+    const amountConverted = {
+      id: 'asdf-1234',
+      userId,
+      originCurrency,
+      originAmount,
+      destinationCurrency,
+      destinationValue: result.amount,
+      currencyTax: result.quote,
+      timeConvert: new Date(),
+    }
+
+    return amountConverted
   }
 }
