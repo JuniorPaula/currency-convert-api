04341a64134bbb99b652384f872fe593b6dd9b1d
Junior Paula <luke.junnior@icloud.com>
2023-01-11 18:08:48 -0300

    feat: ensure TransactionController call TransactionsUsecase with correct values

--- src/presentation/controllers/trasactions-controller.spec.ts
+++ src/presentation/controllers/trasactions-controller.spec.ts
@@ -1,9 +1,29 @@
 import { HttpRequest } from '../protocols/http'
 import { TransactionController } from './transactions-controller'
 
+const mockTransactionsUsecase = () => {
+  class TransactionsUsecaseStub {
+    async load(userId: string) {
+      return null
+    }
+  }
+
+  return new TransactionsUsecaseStub()
+}
+
+const makeSut = () => {
+  const transactionsUsecaseStub = mockTransactionsUsecase()
+  const sut = new TransactionController(transactionsUsecaseStub)
+
+  return {
+    sut,
+    transactionsUsecaseStub,
+  }
+}
+
 describe('TransactionController', () => {
   test('Should return 400 if userId is not provided', async () => {
-    const sut = new TransactionController()
+    const { sut } = makeSut()
 
     const httpRequest: HttpRequest = {
       params: {},
@@ -13,5 +33,17 @@ describe('TransactionController', () => {
     console.log(httpResponse)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toBe('missing param: userId')
+  })
+
+  test('Should call TransactionsUsecase with correct values', async () => {
+    const { sut, transactionsUsecaseStub } = makeSut()
+    const spy = jest.spyOn(transactionsUsecaseStub, 'load')
+    const httpRequest: HttpRequest = {
+      params: {
+        userId: '123',
+      },
+    }
+    await sut.handle(httpRequest)
+    expect(spy).toHaveBeenCalledWith({ userId: '123' })
   })
 })
