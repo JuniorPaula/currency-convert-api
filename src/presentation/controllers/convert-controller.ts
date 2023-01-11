fde7b7b490047633938684bab89fe6f8296cf2f4
Junior Paula <luke.junnior@icloud.com>
2023-01-11 12:58:20 -0300

    feat: ensure ConvertController return 400 if destinationCurrency is not provided

--- src/presentation/controllers/convert-controller.ts
+++ src/presentation/controllers/convert-controller.ts
@@ -5,7 +5,12 @@ import { HttpStatusCodes } from '../utils/http-status-codes'
 
 export class ConventController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const fields = ['userId', 'originCurrency', 'originAmount']
+    const fields = [
+      'userId',
+      'originCurrency',
+      'originAmount',
+      'destinationCurrency',
+    ]
 
     for (const field of fields) {
       if (!httpRequest.body[field]) {
