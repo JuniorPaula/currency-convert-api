7dd029fe6c97912d61567215e507fefe2c90ef0b
Junior Paula <luke.junnior@icloud.com>
2023-01-11 20:20:55 -0300

    test: ensure TransactionController return a transactions on success

--- src/presentation/controllers/trasactions-controller.spec.ts
+++ src/presentation/controllers/trasactions-controller.spec.ts
@@ -4,8 +4,22 @@ import { TransactionController } from './transactions-controller'
 
 const mockTransactionsUsecase = () => {
   class TransactionsUsecaseStub {
+    private userId: string
     async load(userId: string) {
-      return null
+      this.userId = userId
+
+      return Promise.resolve([
+        {
+          trasactionId: '63bf20928a5c7d64453f1462',
+          userId: '0321',
+          originCurrency: 'USD',
+          originAmount: 100,
+          destinationCurrency: 'JPY',
+          destinationValue: 13245.1985,
+          currencyTax: 132.451985,
+          timeConvert: '2023-01-11T20:48:18.523Z',
+        },
+      ])
     }
   }
 
@@ -65,5 +79,30 @@ describe('TransactionController', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return a transactions on success', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest: HttpRequest = {
+      params: {
+        userId: '123',
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual([
+      {
+        trasactionId: '63bf20928a5c7d64453f1462',
+        userId: '0321',
+        originCurrency: 'USD',
+        originAmount: 100,
+        destinationCurrency: 'JPY',
+        destinationValue: 13245.1985,
+        currencyTax: 132.451985,
+        timeConvert: '2023-01-11T20:48:18.523Z',
+      },
+    ])
   })
 })
